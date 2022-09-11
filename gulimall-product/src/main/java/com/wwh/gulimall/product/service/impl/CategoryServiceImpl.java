ec2bbdb1866778d59018d2dbf84a06d9a5c5a403
sxh <1342503240@qq.com>
2022-09-11 23:20:51 +0800

    修改了renren-fast的springboot版本，以及lombok版本,还修改了renren-fast里面一个类,renreen-fast配置了nacos

--- gulimall-product/src/main/java/com/wwh/gulimall/product/service/impl/CategoryServiceImpl.java
+++ gulimall-product/src/main/java/com/wwh/gulimall/product/service/impl/CategoryServiceImpl.java
@@ -1,7 +1,13 @@
 package com.wwh.gulimall.product.service.impl;
 
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
+
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
@@ -16,6 +22,7 @@ import com.wwh.gulimall.product.service.CategoryService;
 @Service("categoryService")
 public class CategoryServiceImpl extends ServiceImpl<CategoryDao, CategoryEntity> implements CategoryService {
 
+
     @Override
     public PageUtils queryPage(Map<String, Object> params) {
         IPage<CategoryEntity> page = this.page(
@@ -26,4 +33,33 @@ public class CategoryServiceImpl extends ServiceImpl<CategoryDao, CategoryEntity
         return new PageUtils(page);
     }
 
+    @Override
+    public List<CategoryEntity> listWithTree() {
+        //1.查出所有分类
+        List<CategoryEntity> entities = baseMapper.selectList(null);
+        //2.组装成父子树形结构
+        List<CategoryEntity> level1Menus = entities.stream().filter(categoryEntity ->
+             categoryEntity.getParentCid() == 0
+        ).map(menu -> {
+            menu.setChildren(getChildren(menu,entities));
+            return menu;
+        }).sorted((menu1,menu2) ->{
+            return menu1.getSort() - menu2.getSort();
+        }).collect(Collectors.toList());
+
+        return level1Menus;
+    }
+
+    //递归设置子菜单
+    private List<CategoryEntity> getChildren(CategoryEntity root,List<CategoryEntity> all){
+        List<CategoryEntity> children = all.stream().filter(categoryEntity -> {
+            return categoryEntity.getParentCid() == root.getCatId();
+        }).map(categoryEntity ->{
+            categoryEntity.setChildren(getChildren(categoryEntity,all));
+            return categoryEntity;
+        }).sorted((menu1,menu2) -> {
+            return (menu1.getSort() == null ? 0 : menu1.getSort()) - (menu2.getSort() == null ? 0 : menu2.getSort());
+        }).collect(Collectors.toList());
+        return children;
+    }
 }
